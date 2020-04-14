72cc4c05853d007c67a4d5efa8882d002f677051
Federico Obialero <federico.obialero@gmail.com>
2020-04-14 17:46:15 +0200

    added avalonian maps

--- formatMaps.js
+++ formatMaps.js
@@ -1,23 +1,31 @@
 const { tiers } = require('./constants')
 
-const categories = ['Solo', 'Group']
+const categories = [
+  { name: 'Solo', id: 'SOLO' },
+  { name: 'Group' },
+  { name: 'Large Group', id: 'ELITE' }
+]
 
 const formatMaps = () =>
   categories
-    .map(category =>
-      tiers.map(tier => {
+    .map((category, idx) => {
+      let tierList = tiers
+      if (idx === 2) {
+        tierList = tiers.slice(2)
+      }
+      return tierList.map(tier => {
         const id =
-          category === 'Solo'
-            ? `${tier.id}_RANDOM_DUNGEON_${category.toUpperCase()}_TOKEN_`
-            : `${tier.id}_RANDOM_DUNGEON_TOKEN_`
-        const name = `${tier.name} Dungeon Map (${category})`
+          category.name === 'Group'
+            ? `${tier.id}_RANDOM_DUNGEON_TOKEN_`
+            : `${tier.id}_RANDOM_DUNGEON_${category.id}_TOKEN_`
+        const name = `${tier.name} Dungeon Map (${category.name})`
         const enchantments = ['1', '2', '3', '4']
         return enchantments.map(ench => ({
           id: id + ench,
           name
         }))
       })
-    )
+    })
     .reduce((curr, next) => [...curr, ...next])
     .reduce((curr, next) => [...curr, ...next])
 
