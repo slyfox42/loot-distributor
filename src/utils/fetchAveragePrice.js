12836b9a47d4ccebbf8dcb317c21816fe8085915
Federico Obialero <federico.obialero@gmail.com>
2020-04-24 09:41:37 +0200

    changed prices time scale from 1 to 24 to get aggregated and more precise data (#24)

--- src/utils/fetchAveragePrice.js
+++ src/utils/fetchAveragePrice.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 const fetchAveragePrice = async (itemID, marketSource, quality = 1) => {
-  const apiUrl = `https://www.albion-online-data.com/api/v2/stats/history/${itemID}?locations=${marketSource}&time-scale=1&qualities=${quality}`
+  const apiUrl = `https://www.albion-online-data.com/api/v2/stats/history/${itemID}?locations=${marketSource}&time-scale=24&qualities=${quality}`
   const result = await fetch(apiUrl).then(res => res.json())
   let averagePrice = 0
 
